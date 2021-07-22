c7a83beaa60075b026f31a22c7b227f36abec8d0
Imane El Mohalil <elmohalilimane@gmail.com>
2021-07-22 02:52:06 +0100

    Moving and Collision Part 2

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    private void FixedUpadate()
+    private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
@@ -25,8 +25,26 @@ public class Player : MonoBehaviour
         if (moveDelta.x > 0)
             transform.localScale = Vector3.one;
         else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 0, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+        // Make sure we can move in this direction by casting a null box
+        hit = Physics2D.BoxCast(transform.position.boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+            // Make Ouss Move <3
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0); // makes the speed of the charachter equal on both a good and a bad device
 
- 
+        }
+
+        hit = Physics2D.BoxCast(transform.position.boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make Ouss Move <3
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0); // makes the speed of the charachter equal on both a good and a bad device
+
+        }
+
+
+
+
     }
 }
